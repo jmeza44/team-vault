28c252b69b354b5a604f67492fed19340d5b5ecd
Jaime Meza <jaimealbertomeza@hotmail.com>
2025-07-28 00:33:38 -0500

    fix: update CORS origin fallback to an empty string

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,7 +20,7 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: process.env['CORS_ORIGIN']?.split(',') || 'http://localhost:5173',
+  origin: process.env['CORS_ORIGIN']?.split(',') || '',
   credentials: true,
 }));
 
